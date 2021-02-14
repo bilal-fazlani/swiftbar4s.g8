3390d5f28f0094c4d0b5e63efbecfb948df8f835
bilal-fazlani <bilal.m.fazlani@gmail.com>
2021-02-14 11:16:36 +0530

    escape $ sign in build-native.sh script

--- src/main/g8/build-native.sh
+++ src/main/g8/build-native.sh
@@ -12,27 +12,27 @@ PLUGIN_FILENAME="\$PLUGIN_NAME.\$REFRESH"
 PLUGINS_DIR="."
 PLUGIN_PATH="\$PLUGINS_DIR"/"\$PLUGIN_FILENAME"
 META_PATH="./src/main/resources/meta.txt"
-TEMP_PLUGIN_PATH="./$PLUGIN_FILENAME"
-JAR_PATH="/tmp/$PLUGIN_NAME.jar"
+TEMP_PLUGIN_PATH="./\$PLUGIN_FILENAME"
+JAR_PATH="/tmp/\$PLUGIN_NAME.jar"
 
 # ----------- PUBLISHING ARTIFACT -------------------------
-rm -rf ~/.ivy2/local/"$ORG"/"$PLUGIN_NAME"_"$SCALA_VERSION"/
+rm -rf ~/.ivy2/local/"\$ORG"/"\$PLUGIN_NAME"_"\$SCALA_VERSION"/
 sbt "publishLocal"
 # ----------- PUBLISHING ARTIFACT END ---------------------
 
 # ------------------ NATIVE -------------------------
 rm -f "$JAR_PATH"
 #Create fat JAR
-coursier bootstrap -f -o "$JAR_PATH" --assembly "$ORG":"$PLUGIN_NAME"_"$SCALA_VERSION":"$PLUGIN_VERSION" --preamble=false
+coursier bootstrap -f -o "\$JAR_PATH" --assembly "\$ORG":"\$PLUGIN_NAME"_"\$SCALA_VERSION":"\$PLUGIN_VERSION" --preamble=false
 #Generate assistive data using agent
-java -agentlib:native-image-agent=config-output-dir=./src/main/resources/META-INF/native-image/ -jar "$JAR_PATH"
+java -agentlib:native-image-agent=config-output-dir=./src/main/resources/META-INF/native-image/ -jar "\$JAR_PATH"
 #Make native image
-rm -f "$TEMP_PLUGIN_PATH"
-native-image --no-fallback -jar "$JAR_PATH" "$TEMP_PLUGIN_PATH"
-chmod +x "$TEMP_PLUGIN_PATH"
-mv "$TEMP_PLUGIN_PATH" "$PLUGIN_PATH"
+rm -f "\$TEMP_PLUGIN_PATH"
+native-image --no-fallback -jar "\$JAR_PATH" "\$TEMP_PLUGIN_PATH"
+chmod +x "\$TEMP_PLUGIN_PATH"
+mv "\$TEMP_PLUGIN_PATH" "\$PLUGIN_PATH"
 # ---------------- NATIVE END ------------------------
 
 # ----------- PUBLISHING METADATA -----------------------
-xattr -w "com.ameba.SwiftBar" $(cat "$META_PATH" | base64) "$PLUGIN_PATH"
+xattr -w "com.ameba.SwiftBar" $(cat "\$META_PATH" | base64) "\$PLUGIN_PATH"
 # ----------- PUBLISHING METADATA END -------------------
